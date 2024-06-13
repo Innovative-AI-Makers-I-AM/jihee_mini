6d722c35c4c53617d290c64ac6c98006c151cf5c
K19619 <157964841+K19619@users.noreply.github.com>
2024-06-13 14:56:27 +0900

    update [박경인] : 출근/퇴근/외출/복귀 기능 구현

--- static/script.js
+++ static/script.js
@@ -22,6 +22,12 @@ const cancelButton = document.getElementById('cancelButton');
 const alreadyExitedModal = document.getElementById('alreadyExitedModal');
 const alreadyExitedMessage = document.getElementById('alreadyExitedMessage');
 const alreadyExitedConfirmButton = document.getElementById('alreadyExitedConfirmButton');
+const newModal = document.querySelector("#new-modal");
+const attendanceBtn = document.querySelector("#attendance");
+const leavingWorkBtn = document.querySelector("#leaving-work");
+const leaveBtn = document.querySelector("#leave");
+const returnBtn = document.querySelector("#return");
+const closeBtn = document.querySelector(".close-btn");
 let currentUserName = '';
 let currentUserInEntryList = false;
 // 웹캠 비디오 스트림을 시작하는 함수
@@ -46,11 +52,8 @@ let isFaceDetected = false;
 function onScriptLoad() {
     // Mediapipe 얼굴 랜드마크 감지 함수
     async function onResults(results) {
-        if (!isFaceDetected && results.multiFaceLandmarks && results.multiFaceLandmarks.length > 0) {
+        if (results.multiFaceLandmarks) {
             isFaceDetected = true; // 얼굴이 감지되면 플래그를 true로 설정
-            const landmarks = results.multiFaceLandmarks[0];
-            // 필요에 따라 landmarks를 처리하거나 저장할 수 있습니다.
-            console.log('Landmarks detected:', landmarks);
 
             // 얼굴이 감지되면 사진을 찍는 함수 호출
             captureButton.click();
@@ -111,8 +114,10 @@ captureButton.addEventListener('click', () => {
                 alreadyExitedMessage.textContent = `${currentUserName}님은 이미 퇴근하셨습니다.`;
                 alreadyExitedModal.style.display = 'block';
             } else {
-                confirmationMessage.textContent = `${currentUserName}님 ${currentUserInEntryList ? '퇴근' : '출근'}확인을 하시겠습니까?`;
-                confirmationModal.style.display = 'block';
+                // confirmationMessage.textContent = `${currentUserName}님 ${currentUserInEntryList ? '퇴근' : '출근'}확인을 하시겠습니까?`;
+                // confirmationModal.style.display = 'block';
+
+                newModal.style.display = 'block';
             }
         } else if (response.status === 404) {
             alert('등록된 사용자가 없어 사용자 등록 페이지로 이동합니다.');
@@ -120,8 +125,14 @@ captureButton.addEventListener('click', () => {
         } else {
             const result = await response.json();
             resultDiv.innerHTML = `<p>${result.detail}</p>`;
+
+            setTimeout(() => {
+                isFaceDetected = false;
+                resultDiv.innerHTML = ''
+            }, 1500); // 몇 초 후에 다시 얼굴 감지 허용
         }
     }, 'image/jpeg');
+
 });
 // 확인 버튼 클릭 이벤트 핸들러
 confirmButton.addEventListener('click', () => {
@@ -154,6 +165,15 @@ function addEntry(name, time) {
     entry.setAttribute('data-name', name);
     entry.setAttribute('data-entry-time', time.toISOString());
     entriesList.appendChild(entry);
+
+    newModal.style.display = 'none';
+    currentUserName = '';
+    currentUserInEntryList = false;
+
+    attendanceBtn.disabled = true;
+    leavingWorkBtn.disabled = false; 
+    leaveBtn.disabled = false;
+    
 }
 // 퇴근 기록 추가 함수
 function addExit(name, exitTime) {
@@ -161,15 +181,132 @@ function addExit(name, exitTime) {
     if (entry) {
         const entryTime = new Date(entry.getAttribute('data-entry-time'));
         const timeString = exitTime.toLocaleTimeString();
-        const totalTime = calculateTotalTime(entryTime, exitTime);
-        entry.textContent += ` / ${timeString} (퇴근) - 총 근무시간: ${totalTime}`;
         entry.setAttribute('data-exit-time', exitTime.toISOString());
+        entry.textContent += ` / ${timeString} (퇴근)`;
+        calculateTotalTime();
     }
+
+    newModal.style.display = 'none';
+    currentUserName = '';
+    currentUserInEntryList = false;
 }
 // 총 근무시간 계산 함수
-function calculateTotalTime(entryTime, exitTime) {
-    const diff = exitTime - entryTime;
-    const hours = Math.floor(diff / (1000 * 60 * 60));
-    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    return `${hours}시간 ${minutes}분`;
+function calculateTotalTime() {
+    const entry = document.querySelector(`#entries li[data-name="${currentUserName}"]`);
+
+    let entryTime = new Date(entry.getAttribute('data-entry-time'));
+    let exitTime = new Date(entry.getAttribute('data-exit-time'));
+    let leaveTime = new Date(entry.getAttribute('data-leave-time'));
+    let returnTime = new Date(entry.getAttribute('data-return-time'));
+
+    if(document.querySelector("li[data-name='sksk']").dataset.returnTime === undefined)
+        returnTime = exitTime;
+    if(document.querySelector("li[data-name='sksk']").dataset.leaveTime === undefined)
+        leaveTime = returnTime;
+
+    let leavedTime = returnTime - leaveTime;
+    let workingTime = exitTime - entryTime;
+
+    // 퇴근 - 출근 - 외출시간(외출~복귀 or 외출~퇴근)
+
+    console.log("entrytime : ", entryTime)
+    console.log("exitTime : ", exitTime)
+    console.log("leaveTime : ", leaveTime)
+    console.log("returnTime : ", returnTime)
+
+    let diff = workingTime - leavedTime;
+    let hours = Math.floor(diff / (1000 * 60 * 60));
+    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+    entry.textContent += ` / 총 근무시간: ${hours}시간 ${minutes}분 ${seconds}초 / `;
+
+    diff = leavedTime;
+    hours = Math.floor(diff / (1000 * 60 * 60));
+    minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    if(document.querySelector("li[data-name='sksk']").dataset.leaveTime === undefined)
+        entry.textContent += `총 외출시간 : 0시간 0분 0초`
+    else
+        entry.textContent += `총 외출시간 : ${hours}시간 ${minutes}분 ${seconds}초`;
+}
+
+
+function addLeave(currentUserName, now){
+    const entry = document.querySelector(`#entries li[data-name="${currentUserName}"]`);
+    if (entry) {
+        const timeString = now.toLocaleTimeString();
+        entry.textContent += ` / ${timeString} (외출)`;
+        entry.setAttribute('data-leave-time', now.toISOString());
+    }
+
+    newModal.style.display = 'none';
+    currentUserName = '';
+    currentUserInEntryList = false;
+
+    leaveBtn.disabled = true; 
+    leavingWorkBtn.disabled = false; 
+    returnBtn.disabled = false; 
+}
+
+function addReturn(currentUserName, now){
+    const entry = document.querySelector(`#entries li[data-name="${currentUserName}"]`);
+    if (entry) {
+        const timeString = now.toLocaleTimeString();
+        entry.textContent += ` / ${timeString} (복귀)`;
+        entry.setAttribute('data-return-time', now.toISOString());
+    }
+
+    newModal.style.display = 'none';
+    currentUserName = '';
+    currentUserInEntryList = false;
+
+    returnBtn.disabled = true;
+
+}
+
+
+attendanceBtn.addEventListener("click", (e)=>{
+    console.log("출근")
+    const now = new Date();
+    addEntry(currentUserName, now);
+
+    
+    setTimeout(() => {
+        isFaceDetected = false;
+    }, 1500); // 몇 초 후에 다시 얼굴 감지 허용
+})
+leavingWorkBtn.addEventListener("click", (e)=>{
+    console.log("퇴근")
+    const now = new Date();
+    addExit(currentUserName, now);
+
+})
+leaveBtn.addEventListener("click", (e)=>{
+    console.log("외출")
+    const now = new Date();
+    addLeave(currentUserName, now);
+
+    setTimeout(() => {
+        isFaceDetected = false;
+    }, 1500); // 몇 초 후에 다시 얼굴 감지 허용
+})
+returnBtn.addEventListener("click", (e)=>{
+    console.log("복귀")
+    const now = new Date();
+    addReturn(currentUserName, now);
+
+    setTimeout(() => {
+        isFaceDetected = false;
+    }, 1500); // 몇 초 후에 다시 얼굴 감지 허용
+})
+
+
+closeBtn.onclick = e=>{
+    console.log("dd")
+    newModal.style.display = 'none';
+
+    setTimeout(() => {
+        isFaceDetected = false;
+    }, 1500); // 몇 초 후에 다시 얼굴 감지 허용
 }

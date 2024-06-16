944d5be9711d766a068350d13230c554664283f0
DESKTOP-0L11UB0\user <ssongjiaran@gmail.com>
2024-06-16 21:27:04 +0900

    add : [송가람] db 연동 중 세부사항 - 변경 사항 -> import 를 특정하게 지정해줌. - 테이블 생성 됨.

--- database/models.py
+++ database/models.py
@@ -1,27 +1,27 @@
-# # database.py에서 연결한 db를 테이블과 매핑시키는 역할
+# database.py에서 연결한 db를 테이블과 매핑시키는 역할
 
-# from sqlalchemy import *
-# from sqlalchemy.orm import relationship
-# from .database import Base
+from sqlalchemy import *
+from sqlalchemy.orm import relationship
+from .database import Base
 
-# class User(Base):
-#     __tablename__ = "user"
+class User(Base):
+    __tablename__ = "user"
 
-#     id = Column(Integer, primary_key=True, index=True)
-#     name = Column(String(255), nullable=False)
-#     embedding = Column(BLOB)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    embedding = Column(BLOB)
 
-#     managing = relationship("Managing", back_populates="user")
+    managing = relationship("Managing", back_populates="user")
 
-# class Managing(Base):
-#     __tablename__ = "managing"
+class Managing(Base):
+    __tablename__ = "managing"
 
-#     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-#     user_id = Column(Integer, ForeignKey("user.id"))
-#     date = Column(Date)
-#     check_in = Column(Time)
-#     out_time = Column(Time)
-#     return_time = Column(Time)
-#     check_out = Column(Time)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    date = Column(Date)
+    check_in = Column(Time)
+    out_time = Column(Time)
+    return_time = Column(Time)
+    check_out = Column(Time)
 
-#     user = relationship("User", back_populates="managing")
+    user = relationship("User", back_populates="managing")
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-# from fastapi import FastAPI, Request
-from fastapi import *
+from fastapi import  FastAPI, Request, Depends, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
 from routers import register, similarity, angle, identify
 # 가람추가
-# from sqlalchemy.orm import Session
-# from database.database import engine, get_db, Base
-# from database.models import User, Managing
+from sqlalchemy.orm import Session
+from database.database import engine, get_db, Base
+from database.models import User, Managing
 # 가람추가
 
 app = FastAPI()
@@ -25,7 +24,7 @@ app.include_router(identify.router)
 
 @app.on_event("startup")
 async def startup_event():
-    # Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
     os.makedirs('data/users', exist_ok=True)
 
 @app.get("/", response_class=HTMLResponse)
